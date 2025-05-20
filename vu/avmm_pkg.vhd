e839727431b2a46af1ccd0e1fb58cd1d5a2f5882
Florian GUGGI (IHV participant) <i11808200@ti22.tilab.tuwien.ac.at>
2025-05-20 09:58:54 +0200

    feat: Use Avmm VU from OSVVM exercise
    With adjusted read wait cycles

--- vu/avmm_pkg.vhd
+++ vu/avmm_pkg.vhd
@@ -5,8 +5,11 @@ library ieee;
 library osvvm;
   context osvvm.OsvvmContext;
 
+library osvvm_common;
+  context osvvm_common.OsvvmCommonContext;
+
 package avmm_pkg is
-  
+
   type AvmmPinoutT is record
     address            : std_logic_vector;
     writedata          : std_logic_vector;
@@ -16,7 +19,32 @@ package avmm_pkg is
     write              : std_logic;
   end record;
 
+  procedure AvmmWrite(signal trans: inout AddressBusRecType; addr, data, byte_enable: std_logic_vector);
+  procedure AvmmRead(signal trans: inout AddressBusRecType; addr, byte_enable: std_logic_vector; variable read_data: out std_logic_vector);
+  procedure AvmmReadModifyWrite(signal trans: inout AddressBusRecType; addr, data, write_mask: std_logic_vector);
+
 end package;
 
 package body avmm_pkg is
+
+  procedure AvmmWrite(signal trans: inout AddressBusRecType; addr, data, byte_enable: std_logic_vector) is
+  begin
+    trans.IntToModel <= to_integer(unsigned(byte_enable));
+    Write(trans, addr, data);
+  end procedure;
+
+  procedure AvmmRead(signal trans: inout AddressBusRecType; addr, byte_enable: std_logic_vector; variable read_data: out std_logic_vector) is
+  begin
+    trans.IntToModel <= to_integer(unsigned(byte_enable));
+    Read(trans, addr, read_data);
+  end procedure;
+
+  procedure AvmmReadModifyWrite(signal trans: inout AddressBusRecType; addr, data, write_mask: std_logic_vector) is
+    variable read_data: std_logic_vector;
+  begin
+    -- Using byteenable would mean we cannot support write_masks that are not byte aligned
+    AvmmRead(trans, addr, (others => '1'), read_data);
+    AvmmWrite(trans, addr, (data and write_mask) or (read_data and not write_mask), (others => '1'));
+  end procedure;
+
 end package body;
--- vu/avmm_vu.vhd
+++ vu/avmm_vu.vhd
@@ -19,4 +19,34 @@ end entity;
 
 architecture rtl of avmm_vu is
 begin
+  sequencer_p: process is
+  begin
+    wait for 0 ns;
+    dispatcher_loop: loop
+      WaitForTransaction(clk => clk_i, Rdy => trans_io.Rdy, Ack => trans_io.Ack);
+      case trans_io.Operation is
+        when WRITE_OP =>
+          pins_io.address <= SafeResize(trans_io.address, pins_io.address'length);
+          pins_io.writedata <= SafeResize(trans_io.DataToModel, pins_io.address'length);
+          pins_io.byteenable <= std_logic_vector(to_unsigned(trans_io.IntToModel, pins_io.byteenable'length));
+          pins_io.write <= '1';
+
+          WaitForClock(clk_i);
+          pins_io.write <= '0';
+
+        when READ_OP =>
+          pins_io.address <= SafeResize(trans_io.address, pins_io.address'length);
+          pins_io.byteenable <= std_logic_vector(to_unsigned(trans_io.IntToModel, pins_io.byteenable'length));
+          pins_io.read <= '1';
+
+          WaitForClock(clk_i, 2);
+          trans_io.DataFromModel <= SafeResize(pins_io.readdata, trans_io.DataFromModel'length);
+          pins_io.read <= '0';
+
+        when others =>
+          Alert("Unimplemented Transaction", FAILURE);
+
+      end case;
+    end loop;
+  end process;
 end architecture;
